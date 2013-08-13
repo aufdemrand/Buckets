7c46ec9bb403bc773c5fe9e3012f92a489d00194
NegroBob <meancraft@hotmail.com>
2013-08-13 00:53:47 -0700

    Another Attempt at Listener Implementation

--- Buckets/src/io/github/NegroBob/Buckets/Buckets.java
+++ Buckets/src/io/github/NegroBob/Buckets/Buckets.java
@@ -5,29 +5,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Buckets extends JavaPlugin {
+
+public class Buckets extends JavaPlugin {
 	
-	@EventHandler()
-	public void onPlayerJoin(PlayerJoinEvent evt) {
-		Player player = evt.getPlayer();
-		PlayerInventory inventory = player.getInventory();
-		ItemStack emptybuckets = new ItemStack(Material.BUCKET, 16);
-		ItemStack fullbuckets = new ItemStack(Material.WATER_BUCKET, 16);
+	
+	@Override
+	public void onEnable(){
 		
-		if (inventory.contains(emptybuckets)) {
-			inventory.removeItem(emptybuckets);
-			inventory.addItem(fullbuckets);
-			player.sendMessage("I have filled your buckets with water!");
-			
-		}else{
-			
-			player.sendMessage("You have no buckets to fill!");
-		}
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
 		
 	}
 	
@@ -48,6 +44,29 @@ public final class Buckets extends JavaPlugin {
 		}
 			return false;
 			
+	}
+	
+	public class Buckets implements Listener {
+
+		@EventHandler
+		public void onPlayerJoin(PlayerJoinEvent evt) {
+			Player player = evt.getPlayer();
+			PlayerInventory inventory = player.getInventory();
+			ItemStack emptybuckets = new ItemStack(Material.BUCKET, 16);
+			ItemStack fullbuckets = new ItemStack(Material.WATER_BUCKET, 16);
+			
+			if (inventory.contains(emptybuckets)) {
+				inventory.removeItem(emptybuckets);
+				inventory.addItem(fullbuckets);
+				player.sendMessage("I have filled your buckets with water!");
+				
+			}else{
+				
+			player.sendMessage("You have no buckets to fill!");
+			
+				}
+			
+			}
 		}
 	
 }
